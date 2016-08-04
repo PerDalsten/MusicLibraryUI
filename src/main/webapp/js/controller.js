2509a8e3bbcd3b7a2f86e94f679211db7b68fc29
Per Dalsten Sorensen <pdalsten@gmail.com>
2016-08-03 21:18:43 -0400

    artist.albums.album

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -3,6 +3,8 @@ app=angular.module("musiclibraryapp", ["config"]);
 
 app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL) {
 	$scope.artists = [];
+	$scope.artistAlbums = [];
+	$scope.album;
 	
 	loadArtists();
 	
@@ -16,5 +18,30 @@ app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL) {
             console.log(response.statusText);
         });
     }
+	
+	$scope.getArtistAlbums = function (artistId){
+		$http({
+            method : 'GET',
+            url : SERVICE_URL+'artists/'+artistId+'/albums'
+        }).then(function successCallback(response) {
+            $scope.artistAlbums = response.data;
+        }, function errorCallback(response) {
+            console.log(response.statusText);
+        });				
+	}
+	
+	$scope.getAlbum = function (albumId){
+		
+		//XXX Loop through artistAlbums instead
+		
+		$http({
+            method : 'GET',
+            url : SERVICE_URL+'albums/'+albumId
+        }).then(function successCallback(response) {
+            $scope.album = response.data;
+        }, function errorCallback(response) {
+            console.log(response.statusText);
+        });
+	}
 	
 });
