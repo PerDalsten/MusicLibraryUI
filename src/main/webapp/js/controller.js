ba41aaad48a5212ec60d1763057c62c5630882ad
Per Dalsten Sorensen <pdalsten@gmail.com>
2016-08-06 19:22:09 -0400

    Ctd. development

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -6,7 +6,10 @@ app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL) {
 
 	$scope.album;
 	$scope.artist;
-
+	$scope.artistName;
+	$scope.albumTitle;
+	$scope.albumYear;
+	
 	loadArtists();
 
 	function loadArtists() {
@@ -29,7 +32,7 @@ app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL) {
 		console.log('Artist changed: ' + $scope.artist.id);
 		$http({
 			method : 'GET',
-			url : SERVICE_URL + 'artists/' + $scope.artist.id + '/albums'
+			url : SERVICE_URL + 'artists/' + $scope.artist.id + '/albums'			
 		}).then(function successCallback(response) {
 			$scope.artistAlbums = response.data;
 		}, function errorCallback(response) {
@@ -46,6 +49,57 @@ app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL) {
 			url : SERVICE_URL + 'albums/' + albumId
 		}).then(function successCallback(response) {
 			$scope.album = response.data;
+		}, function errorCallback(response) {
+			console.log(response.statusText);
+		});
+	}
+	
+	$scope.getAlbums = function() {
+
+		console.log('Artist: '+$scope.artistName);
+		console.log('Album: '+$scope.albumTitle);
+		console.log('Year: '+$scope.albumYear);
+		
+		var url= SERVICE_URL + 'albums';
+		var firstArg = true;
+		
+		if($scope.artistName){			
+			if(firstArg){
+				url+='?';
+				firstArg=false;
+			}else{
+				url+='&';
+			}
+			url+='artist='+$scope.artistName;
+		}
+		
+		if($scope.albumTitle){			
+			if(firstArg){
+				url+='?';
+				firstArg=false;
+			}else{
+				url+='&';
+			}
+			url+='title='+$scope.albumTitle;
+		}
+		
+		if($scope.albumYear){			
+			if(firstArg){
+				url+='?';
+				firstArg=false;
+			}else{
+				url+='&';
+			}
+			url+='year='+$scope.albumYear;
+		}
+		
+		console.log('URL: '+url);
+		
+		$http({
+			method : 'GET',
+			url : url
+		}).then(function successCallback(response) {
+			$scope.artistAlbums = response.data;
 		}, function errorCallback(response) {
 			console.log(response.statusText);
 		});
