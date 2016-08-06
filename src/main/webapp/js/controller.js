f6f1c7574a35561db9ea3b099a3e6be13ccb8c70
Per Dalsten Sorensen <pdalsten@gmail.com>
2016-08-06 10:38:28 -0400

    Artist select

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -1,47 +1,54 @@
-app=angular.module("musiclibraryapp", ["config"]);
-
+app = angular.module("musiclibraryapp", [ "config" ]);
 
 app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL) {
 	$scope.artists = [];
 	$scope.artistAlbums = [];
+
 	$scope.album;
-	
+	$scope.artist;
+
 	loadArtists();
-	
+
 	function loadArtists() {
-        $http({
-            method : 'GET',
-            url : SERVICE_URL+'artists'
-        }).then(function successCallback(response) {
-            $scope.artists = response.data;
-        }, function errorCallback(response) {
-            console.log(response.statusText);
-        });
-    }
-	
-	$scope.getArtistAlbums = function (artistId){
 		$http({
-            method : 'GET',
-            url : SERVICE_URL+'artists/'+artistId+'/albums'
-        }).then(function successCallback(response) {
-            $scope.artistAlbums = response.data;
-        }, function errorCallback(response) {
-            console.log(response.statusText);
-        });				
+			method : 'GET',
+			url : SERVICE_URL + 'artists'
+		}).then(function successCallback(response) {
+			$scope.artists = response.data;
+
+			if ($scope.artists.length > 0) {
+				$scope.artist = $scope.artists[0];
+				getArtistAlbums($scope.artist.id)
+			}
+		}, function errorCallback(response) {
+			console.log(response.statusText);
+		});
 	}
-	
-	$scope.getAlbum = function (albumId){
-		
-		//XXX Loop through artistAlbums instead
-		
+
+	getArtistAlbums = function(artistId) {
+		console.log('Artist changed: ' + $scope.artist.id);
 		$http({
-            method : 'GET',
-            url : SERVICE_URL+'albums/'+albumId
-        }).then(function successCallback(response) {
-            $scope.album = response.data;
-        }, function errorCallback(response) {
-            console.log(response.statusText);
-        });
+			method : 'GET',
+			url : SERVICE_URL + 'artists/' + $scope.artist.id + '/albums'
+		}).then(function successCallback(response) {
+			$scope.artistAlbums = response.data;
+		}, function errorCallback(response) {
+			console.log(response.statusText);
+		});
 	}
-	
+
+	$scope.getAlbum = function(albumId) {
+
+		// XXX Loop through artistAlbums instead
+
+		$http({
+			method : 'GET',
+			url : SERVICE_URL + 'albums/' + albumId
+		}).then(function successCallback(response) {
+			$scope.album = response.data;
+		}, function errorCallback(response) {
+			console.log(response.statusText);
+		});
+	}
+
 });
