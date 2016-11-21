a12e2189420dab166989d37d2c059b851401c855
Per Dalsten Sorensen <pdalsten@gmail.com>
2016-11-20 19:47:22 -0500

    AlbumEdit ctd.

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -56,6 +56,9 @@ app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL, $l
 			if ($scope.albums.length > 0) {
 				$scope.album = $scope.albums[0];
 			}
+			else{
+				$scope.album = null;
+			}
 			
 		}, function errorCallback(response) {
 			console.log(response.statusText);
@@ -121,6 +124,9 @@ app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL, $l
 			if ($scope.albums.length > 0) {
 				$scope.album = $scope.albums[0];
 			}
+			else{
+				$scope.album = null;
+			}
 		}, function errorCallback(response) {
 			console.log(response.statusText);
 		});
@@ -155,7 +161,7 @@ app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL, $l
 				console.log(response.data);
 				
 				var index=findIndexById($scope.artists,$scope.artist.id);	
-				$scope.artists[index].name=response.data.name;	
+				$scope.artists[index].name=response.data.name;					
 				$scope.artist=response.data;
 				
 	        }, function errorCallback(response) {
@@ -190,12 +196,69 @@ app.controller("MusicLibraryController", function($scope, $http, SERVICE_URL, $l
 			$scope.editAlbum.id=$scope.album.id;
 			$scope.editAlbum.title=$scope.album.title;
 			$scope.editAlbum.year=$scope.album.year;
-			$scope.editAlbum.artist=$scope.artist;			
+			$scope.editAlbum.artist=$scope.artist;		
+			
+			$scope.editAlbum.songs = [];
+			
+			copySongs($scope.album.songs, $scope.editAlbum.songs);			
 		}		
 		$location.path('albumedit');
 	}
 	
 	$scope.saveAlbum = function() {
 		console.log('Save Album');	
+				
+		if(!$scope.editAlbum.id){
+			
+			$http.post(SERVICE_URL + 'albums', $scope.editAlbum).then(
+					function successCallback(response) {
+						console.log(response.data);						
+						$scope.albums.push(response.data);
+						$scope.album = response.data;	
+					}, 
+					function errorCallback(response) {
+						console.log(response.statusText);
+					});
+		}else{
+			var url= SERVICE_URL + 'albums/'+$scope.editAlbum.id;
+			
+			$http.put(url, $scope.editAlbum).then(function successCallback(response) {
+				console.log(response.data);
+				
+				var index=findIndexById($scope.albums,$scope.editAlbum.id);
+				console.log('Album index: '+index);
+				$scope.albums[index].title=response.data.title;
+				$scope.albums[index].year=response.data.year;
+				$scope.albums[index].artist=response.data.artist;
+				$scope.albums[index].songs=response.data.songs;
+				$scope.album=response.data;
+				
+	        }, function errorCallback(response) {
+				console.log(response.statusText);
+			});
+			
+		}
+		
+		$location.path('');
+		
+	}
+	
+	$scope.deleteAlbum = function() {
+		console.log('Delete Album');	
+		
+		$http.delete(SERVICE_URL + 'albums/' + $scope.album.id).
+		then(function successCallback(response) {
+			var index=findIndexById($scope.albums,$scope.album.id);	
+			$scope.albums.splice(index, 1);	
+			if($scope.albums.length>0){
+				$scope.album=$scope.albums[0];
+			}
+			else{
+				$scope.album=null;
+			}						
+		},function errorCallback(response) {
+			console.log(response.statusText);
+		});
+		
 	}
 });
--- src/main/webapp/js/util.js
+++ src/main/webapp/js/util.js
@@ -5,3 +5,15 @@ function findIndexById(source, id) {
 		}
 	}
 }
+
+function copySongs(source, target){
+	
+	for (var i = 0; i < source.length; i++) {
+		target[i]={};
+		target[i].id = source[i].id;
+		target[i].title = source[i].title;
+		target[i].track = source[i].track;
+		target[i].disc = source[i].disc;
+	}
+	
+}
